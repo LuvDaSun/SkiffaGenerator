07932d6b881f0f52d8a1ab25674720fe9dd7912c
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-16 21:51:16 +0100

    export middleware

--- packages/ts/oa42-lib/src/main.ts
+++ packages/ts/oa42-lib/src/main.ts
@@ -1,5 +1,6 @@
 export * from "./client/index.js";
 export * from "./errors/index.js";
 export * from "./models/index.js";
+export * from "./server-middleware/index.js";
 export * from "./server/index.js";
 export * from "./utils/index.js";
