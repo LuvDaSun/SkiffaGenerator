401ff9d0935855889d87c99168a019c9c4e15f89
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-01-26 13:54:08 +0100

    implement auth logic

--- packages/ts/oa42-generator/src/generators/bodies/is-authentication.ts
+++ packages/ts/oa42-generator/src/generators/bodies/is-authentication.ts
@@ -1,12 +1,30 @@
 import * as models from "../../models/index.js";
-import { itt } from "../../utils/index.js";
+import { itt, joinIterable, toCamel } from "../../utils/index.js";
 
 export function* generateIsAuthenticationFunctionBody(
   pathModel: models.Path,
   operationModel: models.Operation,
 ) {
-  yield itt`
-    // TODO
-    return true;
-  `;
+  yield itt`return ${joinIterable(generateOrRules(operationModel.authenticationRequirements), " ||\n")}`;
+  return;
+}
+
+function* generateOrRules(requirements: models.AuthenticationRequirement[][]) {
+  if (requirements.length === 0) {
+    yield itt`true`;
+  }
+
+  for (const subRequirements of requirements) {
+    yield joinIterable(generateAndRules(subRequirements), " && ");
+  }
+}
+
+function* generateAndRules(subRequirements: models.AuthenticationRequirement[]) {
+  if (subRequirements.length === 0) {
+    yield itt`true`;
+  }
+
+  for (const requirement of subRequirements) {
+    yield itt`authentication.${toCamel(requirement.authenticationName)} !== undefined`;
+  }
 }
