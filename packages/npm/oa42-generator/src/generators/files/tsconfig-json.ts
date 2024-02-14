48e2aae005a0c405b222a0dd3a1c40a378236aa8
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-02-14 14:48:05 +0100

    wip

--- packages/npm/oa42-generator/src/generators/files/tsconfig-json.ts
+++ packages/npm/oa42-generator/src/generators/files/tsconfig-json.ts
@@ -6,8 +6,6 @@ export function generateTsconfigJsonData() {
       rootDir: "./src",
       sourceMap: true,
       declaration: true,
-      composite: true,
-      lib: ["ES2023", "DOM"],
     },
     include: ["src"],
   };
