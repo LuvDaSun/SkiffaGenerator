0d15f02f7890ba24434bf5ce6ffdab5cc9a64aa7
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-08 22:55:41 +0100

    build scripts

--- packages/npm/oa42-opentelemetry/scripts/build.js
+++ packages/npm/oa42-opentelemetry/scripts/build.js
@@ -3,32 +3,32 @@
 import cp from "child_process";
 import path from "path";
 
-cp.spawnSync("tsc", [], { stdio: "inherit" });
+cp.execFileSync("tsc", [], { shell: true });
 
-cp.spawnSync(
-    "rollup",
-    [
-        "--input",
-        path.resolve("transpiled", "main.js"),
-        "--file",
-        path.resolve("bundled", "main.js"),
-        "--sourcemap",
-        "--format",
-        "es",
-    ],
-    { stdio: "inherit" },
+cp.execFileSync(
+  "rollup",
+  [
+    "--input",
+    path.resolve("transpiled", "main.js"),
+    "--file",
+    path.resolve("bundled", "main.js"),
+    "--sourcemap",
+    "--format",
+    "es",
+  ],
+  { shell: true },
 );
 
-cp.spawnSync(
-    "rollup",
-    [
-        "--input",
-        path.resolve("transpiled", "main.js"),
-        "--file",
-        path.resolve("bundled", "main.cjs"),
-        "--sourcemap",
-        "--format",
-        "cjs",
-    ],
-    { stdio: "inherit" },
+cp.execFileSync(
+  "rollup",
+  [
+    "--input",
+    path.resolve("transpiled", "main.js"),
+    "--file",
+    path.resolve("bundled", "main.cjs"),
+    "--sourcemap",
+    "--format",
+    "cjs",
+  ],
+  { shell: true },
 );
