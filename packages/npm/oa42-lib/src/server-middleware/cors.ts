1aadfe64c31396604ca944d81a79349d0b064046
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-12 19:12:33 +0100

    adds cors (#54)

---
+++ packages/npm/oa42-lib/src/server-middleware/cors.ts
@@ -0,0 +1,39 @@
+import { second } from "msecs";
+import { ServerMiddleware, ServerOutgoingResponse } from "../server/index.js";
+import { methods } from "../utils/index.js";
+
+export interface CorsMiddlewareOptions {
+  /**
+   * maximum age in milliseconds
+   */
+  maxAge: number;
+  /**
+   * allowed origins
+   */
+  allowOrigin: string;
+}
+
+export function createCorsMiddleware(options: CorsMiddlewareOptions): ServerMiddleware {
+  const { allowOrigin } = options;
+  const { maxAge } = options;
+
+  return async function corsMiddleware(request, next) {
+    if (request.method === "OPTIONS") {
+      const response: ServerOutgoingResponse = {
+        status: 204,
+        headers: {
+          "access-control-allow-origin": allowOrigin,
+          "access-control-max-age": (maxAge / second).toFixed(0),
+          "access-control-allow-methods": methods.join(", "),
+          "access-control-allow-headers": "*",
+          "access-control-expose-headers": "*",
+        },
+      };
+      return response;
+    } else {
+      const response = await next(request);
+      response.headers["access-control-allow-origin"] = allowOrigin;
+      return response;
+    }
+  };
+}
