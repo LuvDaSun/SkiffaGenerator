bfecddfa333aaf16ca4f8499b30b2718848eb100
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-05-06 13:55:56 +0200

    fix bug

--- examples/npm/reverse/src/signal.ts
+++ examples/npm/reverse/src/signal.ts
@@ -1,11 +1,13 @@
 export async function waitForSignal(...signals: NodeJS.Signals[]) {
   await new Promise<void>((resolve) => {
-    const abort = () => {
-      for (const signal of signals) {
-        process.removeListener(signal, abort);
-      }
-      resolve();
-    };
+    const abort = () =>
+      // remove listener needs to be performed in next tick to work properly
+      setImmediate(() => {
+        for (const signal of signals) {
+          process.removeListener(signal, abort);
+        }
+        resolve();
+      });
     for (const signal of signals) {
       process.addListener(signal, abort);
     }
