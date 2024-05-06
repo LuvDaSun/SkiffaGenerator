75a6dee1d0ba6fbb42205df52fd971302d52d9c5
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-05-06 20:10:46 +0200

    wip

--- packages/npm/oa42-core/scripts/build.js
+++ packages/npm/oa42-core/scripts/build.js
@@ -1,18 +1,15 @@
 #!/usr/bin/env node
 
 import cp from "child_process";
-import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const projectRoot = path.resolve(dirname, "..");
-const workspaceRoot = path.resolve(projectRoot, "..", "..", "..");
 
-const options = { shell: true, stdio: "inherit", env: process.env };
+const options = { shell: true, stdio: "inherit", env: process.env, cwd: projectRoot };
 
-cp.execFileSync("tsc", ["--project", path.resolve(projectRoot, "tsconfig.json")], options);
-
+cp.execFileSync("tsc", [], options);
 cp.execFileSync("rollup", ["--config", path.resolve(projectRoot, "rollup.config.js")], options);
 
 cp.execFileSync(
@@ -20,9 +17,7 @@ cp.execFileSync(
   ["build", "--package", "oa42-core", "--target", "wasm32-unknown-unknown", "--release"],
   options,
 );
-
 fs.mkdirSync(path.resolve(projectRoot, "bin"), { recursive: true });
-
 fs.copyFileSync(
   path.resolve(workspaceRoot, "target", "wasm32-unknown-unknown", "release", "oa42_core.wasm"),
   path.resolve(projectRoot, "bin", "main.wasm"),
--- packages/npm/oa42-core/scripts/clean.js
+++ packages/npm/oa42-core/scripts/clean.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const projectRoot = path.resolve(dirname, "..");
