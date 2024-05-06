3044292a980336e8faf6cdfa62bbec5f5b0a9da1
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-05-06 19:43:29 +0200

    support sourcmaps better, adds gitattributes

--- packages/npm/oa42-opentelemetry/rollup.config.js
+++ packages/npm/oa42-opentelemetry/rollup.config.js
@@ -1,6 +1,7 @@
 import replace from "@rollup/plugin-replace";
 import path from "path";
 import { defineConfig } from "rollup";
+import sourcemaps from "rollup-plugin-sourcemaps";
 
 const external = (id, parent, resolved) => !(id.startsWith(".") || path.isAbsolute(id));
 
@@ -10,6 +11,7 @@ export default defineConfig([
     input: path.resolve("transpiled", "main.js"),
     output: { file: path.resolve("bundled", "main.js"), format: "module", sourcemap: true },
     plugins: [
+      sourcemaps(),
       replace({
         values: {
           "process.env.NODE_ENV": JSON.stringify("production"),
@@ -23,6 +25,7 @@ export default defineConfig([
     input: path.resolve("transpiled", "main.js"),
     output: { file: path.resolve("bundled", "main.cjs"), format: "commonjs", sourcemap: true },
     plugins: [
+      sourcemaps(),
       replace({
         values: {
           "process.env.NODE_ENV": JSON.stringify("production"),
