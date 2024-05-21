6132291c854dc921ae87fde6b6a789f58501d40f
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-05-21 16:26:08 +0200

    buyild wip[

--- packages/npm/oa42-generator/scripts/build.js
+++ packages/npm/oa42-generator/scripts/build.js
@@ -9,8 +9,8 @@ const projectRoot = path.resolve(dirname, "..");
 
 const options = { shell: true, stdio: "inherit", env: process.env, cwd: projectRoot };
 
-cp.execFileSync("tsc", [], options);
-cp.execFileSync("rollup", ["--config", path.resolve(projectRoot, "rollup.config.js")], options);
-
 cp.execFileSync("npm", ["--workspace", "@oa42/core", "run", "build"], options);
 cp.execFileSync("npm", ["--workspace", "oa42-lib", "run", "build"], options);
+
+cp.execFileSync("tsc", [], options);
+cp.execFileSync("rollup", ["--config", path.resolve(projectRoot, "rollup.config.js")], options);
