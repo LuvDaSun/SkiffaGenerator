3d705f57e00fdb6b496a212d5f7edf71a53fdfde
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-06-01 14:59:16 +0200

    rename

--- packages/cargo/oa42-core/src/utils/mod.rs
+++ packages/cargo/oa42-core/src/utils/mod.rs
@@ -1,11 +1,11 @@
 mod banner;
-mod fetch_file;
+mod fetch_text;
 mod node;
 mod node_cache;
 mod node_location;
 
 pub use banner::*;
-pub use fetch_file::*;
+pub use fetch_text::*;
 pub use node::*;
 pub use node_cache::*;
 pub use node_location::*;
