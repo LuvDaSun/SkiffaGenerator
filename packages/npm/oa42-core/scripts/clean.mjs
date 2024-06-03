709a4dde38969e6b450f9fdf7bbbce8e1ca05cbe
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-06-03 16:52:22 +0200

    wip

--- packages/npm/oa42-core/scripts/clean.mjs
+++ packages/npm/oa42-core/scripts/clean.mjs
@@ -6,7 +6,4 @@ import { fileURLToPath } from "url";
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const projectRoot = path.resolve(dirname, "..");
 
-fs.rmSync(path.resolve(projectRoot, "transpiled"), { recursive: true, force: true });
-fs.rmSync(path.resolve(projectRoot, "typed"), { recursive: true, force: true });
-fs.rmSync(path.resolve(projectRoot, "bundled"), { recursive: true, force: true });
-fs.rmSync(path.resolve(projectRoot, "bin"), { recursive: true, force: true });
+fs.rmSync(path.resolve(projectRoot, "dist"), { recursive: true, force: true });
