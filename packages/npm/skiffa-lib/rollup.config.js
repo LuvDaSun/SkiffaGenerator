06ff69b574198fe7a3210d725e19f3c600b785dc
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-08-07 13:32:22 +0200

    adds browser fields (#96)
    - fixes #95

--- packages/npm/skiffa-lib/rollup.config.js
+++ packages/npm/skiffa-lib/rollup.config.js
@@ -1,7 +1,5 @@
-import replace from "@rollup/plugin-replace";
 import path from "path";
 import { defineConfig } from "rollup";
-import sourcemaps from "rollup-plugin-sourcemaps";
 
 const external = (id, parent, resolved) => !(id.startsWith(".") || path.isAbsolute(id));
 
@@ -11,45 +9,17 @@ export default defineConfig([
     input: path.resolve("transpiled", "main.js"),
     output: { file: path.resolve("bundled", "main.js"), format: "module", sourcemap: true },
     context: "global",
-    plugins: [
-      sourcemaps(),
-      replace({
-        values: {
-          "process.env.NODE_ENV": JSON.stringify("production"),
-        },
-        preventAssignment: true,
-      }),
-    ],
   },
   {
     external,
     input: path.resolve("transpiled", "main.js"),
     output: { file: path.resolve("bundled", "main.cjs"), format: "commonjs", sourcemap: true },
     context: "global",
-    plugins: [
-      sourcemaps(),
-      replace({
-        values: {
-          "process.env.NODE_ENV": JSON.stringify("production"),
-        },
-        preventAssignment: true,
-      }),
-    ],
   },
   {
     external,
     input: path.resolve("transpiled", "browser.js"),
     output: { file: path.resolve("bundled", "browser.js"), format: "module", sourcemap: true },
     context: "window",
-    plugins: [
-      sourcemaps(),
-      replace({
-        values: {
-          "process.env.NODE_ENV": JSON.stringify("production"),
-          "process.browser": JSON.stringify(true),
-        },
-        preventAssignment: true,
-      }),
-    ],
   },
 ]);
