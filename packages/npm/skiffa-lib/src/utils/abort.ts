b565ba962cbb79df46fc7298ed88a2415c120d5a
Elmer Bulthuis <elmerbulthuis@gmail.com>
2025-05-10 11:17:23 +0200

    abort wip

--- packages/npm/skiffa-lib/src/utils/abort.ts
+++ packages/npm/skiffa-lib/src/utils/abort.ts
@@ -1,12 +1,16 @@
 export function setupAbortBubble(parentController: AbortController, childSignal: AbortSignal) {
-  const parentSignal = parentController.signal;
-  const parentAbortHandler = () => {
-    parentSignal.removeEventListener("abort", parentAbortHandler);
-    childSignal.removeEventListener("abort", childAbortHandler);
-  };
-  const childAbortHandler = () => {
+  if (childSignal.aborted) {
     parentController.abort();
-  };
-  parentSignal.addEventListener("abort", parentAbortHandler);
-  childSignal.addEventListener("abort", childAbortHandler);
+  } else {
+    const parentSignal = parentController.signal;
+    const parentAbortHandler = () => {
+      parentSignal.removeEventListener("abort", parentAbortHandler);
+      childSignal.removeEventListener("abort", childAbortHandler);
+    };
+    const childAbortHandler = () => {
+      parentController.abort();
+    };
+    parentSignal.addEventListener("abort", parentAbortHandler);
+    childSignal.addEventListener("abort", childAbortHandler);
+  }
 }
